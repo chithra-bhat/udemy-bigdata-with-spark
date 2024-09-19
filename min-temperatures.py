1bf461f563f2f2ade9753343b5fba52a74024953
Chithra Bhat <bhat.chithra@gmail.com>
2024-09-20 00:37:12 +0530

    Assignment 3 - Find minimum temperature for each station

---
+++ min-temperatures.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+sc = SparkContext(conf = conf)
+
+def parseLines(line):
+	info = line.split(",")
+	stationID = info[0]
+	celsiusTemp = float(info[3]) * 0.1 * (9/5) + 32
+	return (stationID, celsiusTemp)
+
+
+lines = sc.textFile("data/1800.csv")
+
+rdd = lines.filter(lambda x: "TMIN" in x)
+
+minTemps = rdd.map(parseLines)
+
+minTempByStation = minTemps.reduceByKey(lambda x, y: round(min(x, y), 2))
+
+result = minTempByStation.collect()
+
+print("\nStation ID \t Min Temperature in F")
+for key, val in result:
+	print(f"{key} \t {val}")
